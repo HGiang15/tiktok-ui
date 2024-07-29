dbd484f1c106884d71434adb0bbdf6557ccd1a02
HGiang15 <gianghoang150503@gmail.com>
2024-07-29 23:45:44 +0700

    add library fontawesome - and header-search

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,12 +1,38 @@
 import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}></div>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="TikTok" />
+
+                <div className={cx('search')}>
+                    <input
+                        className={cx('search__input')}
+                        type="text"
+                        placeholder="Search account and videos"
+                        spellCheck={false}
+                    />
+
+                    <button className={cx('search__clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+
+                    <FontAwesomeIcon className={cx('search__loading')} icon={faSpinner} />
+
+                    <button className={cx('search__btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+
+                <div className={cx('actions')}></div>
+            </div>
         </header>
     );
 }
